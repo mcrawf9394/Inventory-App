d86ee263f06386eb35c3b5be335e537e1ea70311
mcrawf9394 <mjc9394@gmail.com>
2024-06-06 20:01:48 -0400

    first attempt at pushing to production

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const compression = require("compression")
+const helmet = require("helmet")
 var indexRouter = require('./routes/index');
 var catalogRouter = require('./routes/catalog');
 
@@ -11,7 +12,8 @@ var app = express();
 const mongoose = require("mongoose")
 const {callTracker} = require("assert")
 mongoose.set("strictQuery", false)
-const mongoDb = "mongodb+srv://Admin:hde5ccrsjEfCagq1@cluster0.2xc9jlt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const dev_db_url = "mongodb+srv://Admin:hde5ccrsjEfCagq1@cluster0.2xc9jlt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDb = process.env.MONGODB_URI || dev_db_url;
 main().catch((err) => console.log(err))
 async function main () {
   await mongoose.connect(mongoDb)
@@ -19,11 +21,18 @@ async function main () {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+app.use(limiter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression())
+app.use(helmet())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
